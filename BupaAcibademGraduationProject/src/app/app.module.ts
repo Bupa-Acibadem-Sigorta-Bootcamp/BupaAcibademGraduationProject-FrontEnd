6bd2108dd14c1960418071df441e32ec368c600e
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-06 21:57:22 +0300

    Model class components were created.

--- BupaAcibademGraduationProject/src/app/app.module.ts
+++ BupaAcibademGraduationProject/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { OrderComponent } from './components/order/order.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CardComponent } from './components/card/card.component';
+import { CorporateCustomerComponent } from './components/corporate-customer/corporate-customer.component';
+import { IndividualCustomerComponent } from './components/individual-customer/individual-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    PaymentComponent,
+    OrderComponent,
+    CustomerComponent,
+    CardComponent,
+    CorporateCustomerComponent,
+    IndividualCustomerComponent
   ],
   imports: [
     BrowserModule,
